1907b1aac25438b4251e72349c159f6912db1b61
DavidOngora <ongoradavid5.gmail.com>
2024-03-24 15:07:36 +0300

    correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,6 @@
-# create_school_file.pp
-
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
+
