2faf8b14713d9397df873338e5c60ecdd93544f9
DavidOngora <ongoradavid5.gmail.com>
2024-03-26 16:11:16 +0300

    modifications

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-# installing a package using puppet
+# Install Flask package using pip3
 
-package { 'puppet-lint':
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'], # Ensure that pip3 is installed
+}
+
+package { 'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'gem',
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
+
+# Ensure that python3-pip is installed
+package {'python3-pip':
+  ensure => installed,
 }
